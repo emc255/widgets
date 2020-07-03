3da0685a36ee3815786840e46e3e5e0d84e1a904
emc <micha3l_255@yahoo.com>
2020-07-03 11:59:42 -0500

    Updates

---
+++ precache-manifest.c9ffef75a6a8f8ae2ae4c4058db31934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c4165ddf2cf597c9590f9ea155ced8",
+    "url": "/widgets/index.html"
+  },
+  {
+    "revision": "22d5022958eb5dfce2e0",
+    "url": "/widgets/static/js/2.dd1cb783.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/widgets/static/js/2.dd1cb783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4603dcc8709745b3159",
+    "url": "/widgets/static/js/main.458bcf07.chunk.js"
+  },
+  {
+    "revision": "a3b7248df9ec66a8e8f5",
+    "url": "/widgets/static/js/runtime-main.4d12c3f6.js"
+  }
+]);
